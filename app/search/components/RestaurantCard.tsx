ad33292d92c29bf191b5f2b4320d43d5f4cce8fc
Vasjan Çupri <vasjancupri04@gmail.com>
2023-05-21 18:52:27 +0200

    Fully integrated ratings

--- app/search/components/RestaurantCard.tsx
+++ app/search/components/RestaurantCard.tsx
@@ -2,6 +2,7 @@ import { Cuisine, Location, Review, PRICE } from '@prisma/client'
 import Link from 'next/link'
 import Price from '../../components/Price'
 import { calculateReviewRatingAverage } from '../../../utils/calculateReviewRatingAverage'
+import Stars from '../../components/Stars'
 
 interface Restaurant {
   id: number
@@ -41,7 +42,7 @@ export default function RestaurantCard({
       <div className="pl-5">
         <h2 className="text-3xl">{restaurant.name}</h2>
         <div className="flex items-start">
-          <div className="flex mb-2">*****</div>
+          <Stars reviews={restaurant.reviews}/>
           <p className="ml-2 text-sm">{renderRatingText()}</p>
         </div>
         <div className="mb-9">
